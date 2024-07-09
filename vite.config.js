674f8308bcb2d5707d677590403dfc374df8e425
Maricela Bautista <bautistamaricela2392@gmail.com>
2024-07-08 21:52:44 -0600

    UPDATE:Vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        aboutUs: resolve(__dirname, 'src/pages/acercaDeNosotros/acercadenosotros.html'),
+        products: resolve(__dirname, 'src/pages/productos/productos.html'),
+        contact: resolve(__dirname, 'src/pages/contacto/contacto.html'),
+      },
+    },
+  },
+})
